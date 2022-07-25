ba59220221c6b3281fc4aef95ac6156de95c70b5
zhentao1123 <zzt1981@126.com>
2022-07-25 12:27:09 +0800

    2 files modified

--- src/main.js
+++ src/main.js
@@ -2,11 +2,9 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import Antd from 'ant-design-vue';
 import './styles/index.scss';
-import 'ant-design-vue/dist/antd.css';
 
 //路由鉴权处理
 import '@/permission';
 
-createApp(App).use(Antd).use(router).use(store).mount('#app');
+createApp(App).use(router).use(store).mount('#app');
